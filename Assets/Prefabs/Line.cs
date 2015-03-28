10931b0c60fc537dc4494bd88fcf19d30e365cdf
likangning93 <likang+bitbucket@seas.upenn.edu>
2015-03-28 16:13:33 -0400

    ok working on splines commit fo real. my bad

---
+++ Assets/Prefabs/Line.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// from http://catlikecoding.com/unity/tutorials/curves-and-splines/
+
+public class Line : MonoBehaviour {
+
+	public Vector3 p0, p1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
